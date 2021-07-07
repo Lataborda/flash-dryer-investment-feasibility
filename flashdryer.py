ff508c73b0e49763b117b88166ebdb9476d83898
Lataborda <83258732+Lataborda@users.noreply.github.com>
2021-07-07 12:45:27 -0500

    Update flashdryer.py

--- flashdryer.py
+++ flashdryer.py
@@ -197,7 +197,7 @@ disp_col.markdown(round(CPC,2))
 sel_col.write('**Please specify the percentage of participation of the type of origin of the investment**')
 Owm_Resourses = sel_col.selectbox('Own resources(in %)', options=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100], index=0)
 Bank_loan = 100-(int(Owm_Resourses))
-
+sel_col.write(Bank_Loan)
 
 if int(Owm_Resourses)!=0:
 	opportunity_rate = sel_col.slider('what is the annual Opportunity cost of the investment(in %)', min_value=1, max_value=25, value=3, step=1)
